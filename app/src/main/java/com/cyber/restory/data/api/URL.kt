8c2d9e4faa64e90399c9e64145ffc43b6ac2f4c3
psm0102 <psmapp0102@naver.com>
2024-09-13 23:19:45 +0900

    # URL - 주석 추가

--- app/src/main/java/com/cyber/restory/data/api/URL.kt
+++ app/src/main/java/com/cyber/restory/data/api/URL.kt
@@ -2,8 +2,14 @@ package com.cyber.restory.data.api
 
 import com.cyber.restory.BuildConfig
 
+
 object URL {
+    // 개발 도메인
     const val BASE_URL = BuildConfig.API_URL
+
+    // 카카오맵 API KEY
     const val KAKAO_API = BuildConfig.API_KEY
+
+    // 한국관광공사 API KEY
     const val GREEN_TOUR_API = BuildConfig.GREEN_TOUR_API_KEY
 }
