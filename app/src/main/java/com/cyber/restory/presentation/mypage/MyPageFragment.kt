26695057590a8be5b7f490aab4a23079f672c99c
StopStone <wltjr6432@naver.com>
2024-09-26 21:52:52 +0900

    설정 화면 문의 기능 추가

--- app/src/main/java/com/cyber/restory/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/cyber/restory/presentation/mypage/MyPageFragment.kt
@@ -1,5 +1,7 @@
 package com.cyber.restory.presentation.mypage
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -34,7 +36,7 @@ class MyPageFragment : Fragment() {
         }
 
         binding.sivServiceInquiry.setOnClickListener {
-
+            sendServiceInquiryEmail()
         }
 
         binding.sivShareService.setOnClickListener {
@@ -44,6 +46,28 @@ class MyPageFragment : Fragment() {
         binding.sivNotice.setOnClickListener {
             val action = MyPageFragmentDirections.actionMypageToNotice()
             findNavController().navigate(action)
+        }
+    }
+
+    private fun sendServiceInquiryEmail() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "message/rfc822"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("dev.stopstone@gmail.com"))
+            putExtra(Intent.EXTRA_SUBJECT, "서비스 문의")
+            putExtra(Intent.EXTRA_TEXT, "여기에 문의 내용을 작성해주세요.")
+        }
+
+        try {
+            val gmailIntent = intent.apply {
+                setPackage("com.google.android.gm")
+            }
+            startActivity(gmailIntent)
+        } catch (e: ActivityNotFoundException) {
+            try {
+                startActivity(Intent.createChooser(intent, "이메일 전송하기"))
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(context, "이메일 앱을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
