61ce0b2fb544d8f38ba092419dbb9b5c6fdd93d2
StopStone <wltjr6432@naver.com>
2024-09-26 21:14:54 +0900

    설정 화면 서비스 공유 - 서비스 공유 준비중 토스트

--- app/src/main/java/com/cyber/restory/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/cyber/restory/presentation/mypage/MyPageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cyber.restory.databinding.FragmentMypageBinding
@@ -36,7 +37,7 @@ class MyPageFragment : Fragment() {
         }
 
         binding.sivShareService.setOnClickListener {
-            // Handle share service click
+            Toast.makeText(context, "준비중입니다.", Toast.LENGTH_SHORT).show()
         }
 
         binding.sivNotice.setOnClickListener {
